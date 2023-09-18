4008bcbef9f64f6f00834e6f4227d12ecbb807ad
June Hansen <44864244+Kahera@users.noreply.github.com>
2023-09-18 23:35:24 +0200

    Made resume view suppoert i18n

--- src/views/ResumeView.vue
+++ src/views/ResumeView.vue
@@ -1,42 +1,65 @@
 <script setup lang="ts">
+
+// Vue
+import { computed, ref, watch } from 'vue';
+
 // Components
 import Button from '@/components/ButtonComponent.vue';
 import ListItem from '@/components/view-experience/ListItem.vue'
 import CVItem from '@/components/view-experience/CVItem.vue';
 
-// Data
-import jobData from '@/assets/data/jobs.json';
-import educationData from '@/assets/data/education.json';
-import { computed, ref } from 'vue';
-
 // Animations
 import gsap from 'gsap';
 import { usePrefersReducedMotion } from '@/utilities/prefers-reduced-motion';
 
+// Data
+import jobsNo from "@/assets/data/jobs-no.json";
+import jobsEn from "@/assets/data/jobs-en.json";
+import educationNo from '@/assets/data/education-no.json';
+import educationEn from '@/assets/data/education-en.json';
 
-// sort data by end date - most recent first, with current jobs at the top
-const jobs = jobData.sort((a, b) => {
-  if (a.endDate === undefined) return -1;
-  else if (b.endDate === undefined) return 1;
-  else return new Date(b.endDate).getUTCMilliseconds() - new Date(a.endDate).getUTCMilliseconds();
+import { useI18n } from "vue-i18n";
+const i18nLocale = useI18n();
+
+let jobs = ref(<typeof jobsEn>[]);
+let education = ref(<typeof educationEn>[]);
+updateData(i18nLocale.locale.value);
+
+watch(() => i18nLocale.locale.value, (newLocale, _) => {
+  updateData(newLocale);
 });
 
-const educations = educationData.sort((a, b) => {
-  if (a.endDate === undefined) return -1;
-  else if (b.endDate === undefined) return 1;
-  else return new Date(b.endDate).getUTCMilliseconds() - new Date(a.endDate).getUTCMilliseconds();
-});
+function updateData(locale: string) {
+  if (locale == "en") {
+    jobs.value = jobsEn;
+    education.value = educationEn;
+  } else {
+    jobs.value = jobsNo;
+    education.value = educationNo;
+  }
+}
+
+// Sort data by end date - most recent first, with current jobs at the top
+function sortDataByEndDate(array: any[]): any[] {
+  return array.sort((a, b) => {
+    if (a.endDate === undefined) return -1;
+    else if (b.endDate === undefined) return 1;
+    else return new Date(b.endDate).getUTCMilliseconds() - new Date(a.endDate).getUTCMilliseconds();
+  });
+}
 
 const displayAllJobs = ref(false);
 
-const displayJobs = computed(() => {
-  if (displayAllJobs.value) return jobs;
-  else return jobs.slice(0, 2);
+const educations = computed(() => sortDataByEndDate(education.value) as typeof educationEn);
+const jobsSorted = computed(() => sortDataByEndDate(jobs.value) as typeof jobsEn);
+const jobsToDisplay = computed(() => {
+  if (displayAllJobs.value) return jobsSorted.value;
+  else return jobsSorted.value.slice(0, 2);
 });
+
 
 // Animations
 const reducedMotion = usePrefersReducedMotion();
-
 function onEnter(el: any, done: any) {
   gsap.fromTo(el, {
     opacity: 0,
@@ -51,7 +74,7 @@ function onEnter(el: any, done: any) {
 }
 
 function onLeave(el: any, done: any) {
-  const delayModifier = el.dataset.type === 'job' ? jobs.length : educations.length;
+  const delayModifier = el.dataset.type === 'job' ? jobsSorted.value.length : educations.value.length;
   gsap.to(el, {
     opacity: 0,
     height: 0,
@@ -70,7 +93,7 @@ function onLeave(el: any, done: any) {
 
       <ol class="flex flex-col space-y-6">
         <TransitionGroup name="list" @enter="onEnter" @leave="onLeave">
-          <CVItem v-for="(job, index) in displayJobs" :key="index" :data-index="index" :data-type="'job'"
+          <CVItem v-for="(job, index) in jobsToDisplay" :key="index" :data-index="index" :data-type="'job'"
             :startDate="new Date(job.startDate)" :endDate="job.endDate ? new Date(job.endDate) : undefined"
             :company="job.company" :url="job.url" :position="job.position">
             <template #description><span class="whitespace-pre-wrap">{{ job.description }}</span></template>
