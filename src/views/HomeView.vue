234aeef4074f0c562b767e6b4b6cb66789efd44a
June Hansen <44864244+Kahera@users.noreply.github.com>
2023-09-18 23:37:20 +0200

    made paths on homepage translated

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,7 +4,7 @@
 <template>
   <div class="flex w-full h-full justify-between">
     <RouterLink v-for="route in $router.getRoutes()" :key="route.path" :to="route.path">
-      {{ route.name }}
+      {{ $t('pages.' + route.name?.toString()) }}
     </RouterLink>
   </div>
 </template> 
