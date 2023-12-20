e285e912877203174d1403c7ffa1138b1666ef8b
June Hansen <44864244+Kahera@users.noreply.github.com>
2023-12-20 23:30:58 +0100

    removed 'under construction' label for deployed version

--- src/App.vue
+++ src/App.vue
@@ -7,15 +7,6 @@ const isDev = import.meta.env.DEV;
 </script>
 
 <template>
-  <!-- Under construction indicator for deployed version -->
-  <p
-    v-if="!isDev"
-    class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 -rotate-45
-  text-center text-danger opacity-90 font-bold text-6xl pointer-events-none"
-  >
-    {{ $t('warnings.underConstruction') }}
-  </p>
-
   <NavbarComponent />
 
   <!-- Main page content -->
